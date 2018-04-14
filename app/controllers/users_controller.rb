2633f737c83cdca9a6e331e296e8d98bd813bf1b
Noah Callahan <noahc1991@gmail.com>
2018-04-14 15:37:05 -0400

    home page, create account, user list, user profile with users posts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+    redirect_to '/'
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    
+  end
+
+  def name
+    
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:uname, :fname, :lname, :email, :password)
+  end
+
+end
+
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create(uname:"noahc", fname:"Noah", lname:"Callahan", email:"noahc1@gmail.com", password:"nycda")
+# User.create(uname:"testm", fname:"Test", lname:"Callahan", email:"testm@gmail.com", password:"nycda")
+# User.create(uname:"tima", fname:"Tim", lname:"Allen", email:"tima@gmail.com", password:"nycda")
+
+
+# Post.create(title: "Testing Post", content:"database seed posts", user_id:1)
+# Post.create(title: "Testing Post", content:"database seed posts", user_id:2)
+# Post.create(title: "Testing Post", content:"database seed posts", user_id:3)
+
+# Post.create(title: "Testing Post 2", content:"database seed posts #2", user_id:1)
+# Post.create(title: "Testing Post 2", content:"database seed posts #2", user_id:2)
+# Post.create(title: "Testing Post 2", content:"database seed posts #2", user_id:3)
+
+#Post.create(title: "Testing Post 2", content:"database seed posts #2", user_id:1)
