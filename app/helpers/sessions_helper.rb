29b39c2a5ad1d601ad86fe9ad4fe5ee24f11e3bf
Noah Callahan <noahc1991@gmail.com>
2018-04-14 22:10:57 -0400

    users can now login, stay logged in, direct to their profile area, view all users when logged in, logout

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,23 @@
+module SessionsHelper
+    #login
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if @current_user.nil?
+       @current_user = User.find_by(id: session[:user_id])
+    else
+      @current_user
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session[:user_id] = nil
+    @current_user = nil
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   get '/' => 'welcome#index'
   get '/signup' => 'users#new'
   root 'welcome#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
