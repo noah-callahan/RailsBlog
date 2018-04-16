ec36ac71d9292c2c374d12e050c267f27685b96a
Noah Callahan <noahc1991@gmail.com>
2018-04-16 17:11:28 -0400

    edit account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
   def name
     
   end
