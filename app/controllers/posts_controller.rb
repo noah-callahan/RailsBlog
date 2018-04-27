d34a833659c4249080c19de152d5bb3f2087d4f9
Noah Callahan <noahc1991@gmail.com>
2018-04-26 22:03:07 -0400

    route fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(update_params)
+    redirect_to user_path(current_user)
   end
 
   def destroy
